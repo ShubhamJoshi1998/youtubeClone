7f072ba80f3e33ed7ee51fe6ce9f3ae399d228ac
shubham <shivam306joshi@gmail.com>
2026-05-05 15:33:59 +0530

    first commit

---
+++ src/Components/Recomended/Recomended.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import './Recomended.css'
+import thumbnail1 from '../../assets/thumbnail1.png'
+import thumbnail2 from '../../assets/thumbnail2.png'
+import thumbnail3 from '../../assets/thumbnail3.png'
+import thumbnail4 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+import thumbnail8 from '../../assets/thumbnail8.png'
+
+const Recomended = () => {
+  return (
+    <div className='recomended'>
+        <div className='side-vide-list'>
+            <img src={thumbnail1} alt=''/>
+            <div className='vid-info'>
+                <h4>Best Youtube Channel to learn Web Development</h4>
+                <p>Great Stack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+        <div className='side-vide-list'>
+            <img src={thumbnail2} alt=''/>
+            <div className='vid-info'>
+                <h4>Best Youtube Channel to learn Web Development</h4>
+                <p>Great Stack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+        <div className='side-vide-list'>
+            <img src={thumbnail3} alt=''/>
+            <div className='vid-info'>
+                <h4>Best Youtube Channel to learn Web Development</h4>
+                <p>Great Stack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+        <div className='side-vide-list'>
+            <img src={thumbnail4} alt=''/>
+            <div className='vid-info'>
+                <h4>Best Youtube Channel to learn Web Development</h4>
+                <p>Great Stack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+        <div className='side-vide-list'>
+            <img src={thumbnail5} alt=''/>
+            <div className='vid-info'>
+                <h4>Best Youtube Channel to learn Web Development</h4>
+                <p>Great Stack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+        <div className='side-vide-list'>
+            <img src={thumbnail6} alt=''/>
+            <div className='vid-info'>
+                <h4>Best Youtube Channel to learn Web Development</h4>
+                <p>Great Stack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+        <div className='side-vide-list'>
+            <img src={thumbnail7} alt=''/>
+            <div className='vid-info'>
+                <h4>Best Youtube Channel to learn Web Development</h4>
+                <p>Great Stack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+        <div className='side-vide-list'>
+            <img src={thumbnail8} alt=''/>
+            <div className='vid-info'>
+                <h4>Best Youtube Channel to learn Web Development</h4>
+                <p>Great Stack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Recomended
---
+++ src/Pages/Video/Video.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Video.css'
+import PlayVideo from '../../Components/PlayVideo/PlayVideo';
+import Recomended from '../../Components/Recomended/Recomended';
+
+const Video = () => {
+  return (
+    <div className='play-container'>
+        <PlayVideo />
+        <Recomended />
+
+    </div>
+  )
+}
+
+export default Video;
